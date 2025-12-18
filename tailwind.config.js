fa8e6d906b9ae7d7c9426ad4cac3365ee62fdcc2
Old <225056366+roy-henderson@users.noreply.github.com>
2025-12-18 14:45:20 -0500

    feat: Add tailwind.config.js | Dec 17, 2025 9:03 PM EST

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        emerald: {
+          400: '#34d399',
+          500: '#10b981',
+        },
+      },
+    },
+  },
+  plugins: [],
+}

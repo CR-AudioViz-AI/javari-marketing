f4b941d3c85b614721dd8ac525738ebe064a6844
Old <225056366+roy-henderson@users.noreply.github.com>
2025-12-31 11:27:10 -0500

    Fix TikTok verification content

--- app/tiktokNyWJKWegGV3ZSHSKAPILeNC6P3TIBDdr.txt/route.ts
+++ app/tiktokNyWJKWegGV3ZSHSKAPILeNC6P3TIBDdr.txt/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-  return new NextResponse('tiktokNyWJKWegGV3ZSHSKAPILeNC6P3TIBDdr', {
+  return new NextResponse('tiktok-developers-site-verification=NyWJKWegGV3ZSHSKAPlLeNC6P3TIBDdr', {
     headers: {
       'Content-Type': 'text/plain',
     },

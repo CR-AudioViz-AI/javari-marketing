63f49cffc23db8f83d0c286fe5550b019f4cdf1e
Old <225056366+roy-henderson@users.noreply.github.com>
2026-05-16 13:16:20 -0400

    fix: ignoreBuildErrors [javari-marketing] (#37)

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
-  reactStrictMode: false,
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  },
-}
+const nextConfig = {typescript:{ignoreBuildErrors:true},eslint:{ignoreDuringBuilds:true},reactStrictMode:false}
 module.exports = nextConfig

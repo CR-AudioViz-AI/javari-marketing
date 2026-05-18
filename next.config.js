dadf5b1f5d4cf53cfd2a20c747f1cc4e5acebda1
Old <225056366+roy-henderson@users.noreply.github.com>
2026-05-18 16:21:58 -0400

    chore: trigger rebuild [javari-marketing] 1779135717

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+// Build trigger: 1779135717
 /** @type {import("next").NextConfig} */
 const nextConfig={typescript:{ignoreBuildErrors:true},eslint:{ignoreDuringBuilds:true},reactStrictMode:false}
 module.exports=nextConfig
